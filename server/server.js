89f325569ced87930a182e843ca4dea15763628f
Daichi Nishida <verhichi@hotmail.com>
2018-11-15 00:51:22 +0900

    Import "socket.io", "socket.io-client", "@types/socket.io-client" to project. Write placeholder socket.io code.
    -

--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
-const express = require('express');      // import express framework
-const app = express();                   // instantiating express
-const portNo = process.env.PORT || 3000; // set port number
+const express = require('express');           // import express framework
+const app     = express();                    // instantiating express
+const http    = require('http');              // import http package
+const server  = http.Server(app);             // Create server
+const io      = require('socket.io')(server); // set socket.io with server
+
+const portNo  = process.env.PORT || 3000; // set port number
 
 const path = require('path');            // import path object
 
@@ -9,7 +13,7 @@ app.use(body_parser.urlencoded({extended: true})); // use body-parser
 app.use(body_parser.json());                       // parse body to json
 
 // Set express listener at port 3000
-var listener = app.listen(portNo, () => {
+var listener = server.listen(portNo, () => {
   const server_start_timestamp = new Date().toLocaleString();
 
   console.log('\n--- SERVER STATUS ---');
@@ -20,11 +24,20 @@ var listener = app.listen(portNo, () => {
 });
 
 
-
 // Set path for file to ./dist
 app.use(express.static(path.join(__dirname, '../dist')));
 
 // All URL sends user to home page
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../dist/index.html'));
+});
+
+
+// Place holder socket.io logic
+io.on('connection', (socket) => {
+  console.log('A user has connected to a socket', socket.client.id);
+
+  socket.on('msg', (msg) => {
+    io.emit('msg', 'msg sent back from server' + msg);
+  })
 });
