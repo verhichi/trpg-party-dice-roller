37b342170a52ad25230d7fa9ea83be5cc55a60b4
Daichi Nishida <verhichi@hotmail.com>
2018-11-15 01:04:43 +0900

    Add placeholder codes for socket.io-client in "web-socket.server.ts".
    -

--- src/app/service/web-socket.service.ts
+++ src/app/service/web-socket.service.ts
@@ -1,8 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import * as io from 'socket.io-client';
 
 @Injectable()
 export class WebSocketService {
 
-  constructor() { }
+  private socket: SocketIOClient.Socket;
+
+  constructor() {
+    this.socket = io('http://localhost:3000');
+  }
+
+  sendMessage(msg: string){
+    this.socket.emit('msg', {msg: msg});
+  }
+
+  onNewMessage(){
+    return Observable.create(observer => {
+      this.socket.on('msg', msg => {
+        observer.next(msg);
+      });
+    });
+  }
 
 }
