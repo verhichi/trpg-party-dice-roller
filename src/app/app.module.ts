f1670a90d05a80ace28ebabc307dc8913c5fd00b
Daichi Nishida <verhichi@hotmail.com>
2018-11-14 16:33:24 +0900

    Generate Components(without spec file): "header","body","lobby","join-room","host-room","room","self-user","other-user","log" using angular-cli. Removed default place holder html in "app.component.html".
    -

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,29 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BodyComponent } from './body/body.component';
+import { LobbyComponent } from './body/lobby/lobby.component';
+import { RoomComponent } from './body/room/room.component';
+import { JoinRoomComponent } from './body/lobby/join-room/join-room.component';
+import { HostRoomComponent } from './body/lobby/host-room/host-room.component';
+import { SelfUserComponent } from './body/room/self-user/self-user.component';
+import { OtherUserComponent } from './body/room/other-user/other-user.component';
+import { LogComponent } from './body/room/log/log.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyComponent,
+    LobbyComponent,
+    RoomComponent,
+    JoinRoomComponent,
+    HostRoomComponent,
+    SelfUserComponent,
+    OtherUserComponent,
+    LogComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/body/lobby/host-room/host-room.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-host-room',
+  templateUrl: './host-room.component.html',
+  styleUrls: ['./host-room.component.css']
+})
+export class HostRoomComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
